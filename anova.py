5abb2f2ffb4d56c069ae5b1c6855886f7441f694
Jordi Bosch <jordiboschbosch@hotmail.com>
2022-05-12 19:26:54 +0200

    Add anova charting

--- anova.py
+++ anova.py
@@ -1,6 +1,5 @@
+import os
 from parseInput import parseInput
-from flask_restful import Resource, reqparse
-from flask import request
 import numpy as np
 from scipy.stats import f_oneway
 import tempfile
@@ -16,12 +15,18 @@ class Anova():
         data_boxplot = []
         for treatment in list:
             data_boxplot.append(treatment)
-        plt.boxplot(data_boxplot)
+        treatments = []
+        for counter, treatment in enumerate(list):
+            treatments = treatments + \
+                ['Treatment' + str(counter + 1)]*len(treatment)
+        flat_list = [item for sublist in list for item in sublist]
+        datos = pd.DataFrame(
+            {'Treatments': treatments, 'Quantitative Variable': flat_list})
+        fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+        sns.boxplot(x="Treatments", y="Quantitative Variable",
+                    data=datos, ax=ax)
+        sns.swarmplot(x="Treatments", y="Quantitative Variable",
+                      data=datos, color='black', alpha=0.5, ax=ax)
         plt.title("Boxplot")
         tmpdir = tempfile.gettempdir()
         plt.savefig(tmpdir + '/anova.png')
-
-    def computePValue(list):
-        from scipy.stats import f_oneway
-        # perform one-way ANOVA
-        print(f_oneway(*list))
